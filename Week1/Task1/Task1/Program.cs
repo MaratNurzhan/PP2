7f33fd88059958f3aa47c7470152648cdf11f345
Nurzhan <nurzhan.marat.2000@mail.ru>
2019-01-26 14:44:49 +0600

    init

---
+++ Week1/Task1/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] arr = Console.ReadLine().Split();
+
+            int[] arr1 = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int n1 = int.Parse(arr[i]);
+                arr1[i] = n1;
+            }
+            int cnt = 0;
+            List<int> list = new List<int>();
+            for(int i=0; i<arr1.Length; i++)
+            {
+                if (isPrime(arr1[i]))
+                {
+                    cnt++;
+                    list.Add(arr1[i]);
+                }
+            }
+            Console.WriteLine(cnt);
+            foreach (int item in list)
+                Console.Write(item + " ");
+
+            Console.ReadKey();
+        }
+
+        static bool isPrime(int n)
+        {
+            if (n <= 0) return false;
+            if (n == 1) return false;
+            if (n == 2) return true;
+            for(int i = 2; i <= Math.Ceiling(Math.Sqrt(n)); i++)
+                if (n % i == 0) return false;
+
+            return true;
+        }
+    }
+}
