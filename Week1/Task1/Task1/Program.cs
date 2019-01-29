aeb70c87f1e87e7016331f22662f09d415917abe
Nurzhan <nurzhan.marat.2000@mail.ru>
2019-01-29 21:36:07 +0600

    Comments

--- Week1/Task1/Task1/Program.cs
+++ Week1/Task1/Task1/Program.cs
@@ -10,12 +10,18 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            //Вводимые данные перевожу в тип int и помещяю в переменной n
             int n = int.Parse(Console.ReadLine());
-            string[] arr = Console.ReadLine().Split();
-
+            // string str = Console.ReadLine();
+            //string[] arr = str.Split();
+             string[] arr = Console.ReadLine().Split();
+           
+           //Выделяю память для нового массива arr1 длиной n
             int[] arr1 = new int[n];
             for (int i = 0; i < arr.Length; i++)
             {
+                //Пробегаясь по элементам массива передаем в переменную n1
+                //в новый массив arr1 передаю элементы n1
                 int n1 = int.Parse(arr[i]);
                 arr1[i] = n1;
             }
@@ -25,7 +31,9 @@ namespace Task1
             {
                 if (isPrime(arr1[i]))
                 {
+                    
                     cnt++;
+                    //Добавляю в List простые числа
                     list.Add(arr1[i]);
                 }
             }
@@ -36,15 +44,20 @@ namespace Task1
 
             Console.ReadKey();
         }
-
+        //Создаю функцию isPrime, которая возвращает true или false 
+        //в зависомости от получаемого значения в качестве аргумента n
         static bool isPrime(int n)
         {
+            //Отрицательные значения не рассматриваются
             if (n <= 0) return false;
+            //в случае полученя 1 или 2 мишем возвращаемый результат отдельно, так как 
+            //цикл будет пробегаться от 2 до 1 или от 2 до 2(здесь 2 делится на 2, будет false, должно быть true)
             if (n == 1) return false;
             if (n == 2) return true;
+            //Число Prime, если оно не делится на числа меньше квадратного корня данного числа
             for(int i = 2; i <= Math.Ceiling(Math.Sqrt(n)); i++)
                 if (n % i == 0) return false;
-
+            //Если число проходит проверку(не делится) то возвращаю true
             return true;
         }
     }
