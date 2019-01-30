21a4d0533eda0b60357aa1c3ebff25899b5e7279
Nurzhan <nurzhan.marat.2000@mail.ru>
2019-01-30 11:51:03 +0600

    Comments_1

--- Week1/Task1/Task1/Program.cs
+++ Week1/Task1/Task1/Program.cs
@@ -10,54 +10,65 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            //Вводимые данные перевожу в тип int и помещяю в переменной n
+            //convert the input data into int type to place it into int variable n
             int n = int.Parse(Console.ReadLine());
             // string str = Console.ReadLine();
             //string[] arr = str.Split();
+            //Split the string according to the condition in commas of "Split()", if it is empty
+            //devide by gaps
+            //Save it into array od strings
              string[] arr = Console.ReadLine().Split();
            
-           //Выделяю память для нового массива arr1 длиной n
+           //allocate memeory for array arr1 of length n
             int[] arr1 = new int[n];
             for (int i = 0; i < arr.Length; i++)
             {
-                //Пробегаясь по элементам массива передаем в переменную n1
-                //в новый массив arr1 передаю элементы n1
+                //Convert every arr[i] element into int type because arr is array of strings
+                //Running through every arr[i] element put them in variable n1
+                //as here are several elements we must put every n1 into arr1 array while for(cycle)is implemented
+                
+                
                 int n1 = int.Parse(arr[i]);
                 arr1[i] = n1;
             }
+            //variable counter
             int cnt = 0;
+            //Create list to save all prime numbers in it
+            //The amount of prime numbers is unknown,that is why I use List(its size change dynamically)
             List<int> list = new List<int>();
             for(int i=0; i<arr1.Length; i++)
             {
+                //Call function "isPrime" for each element of array
                 if (isPrime(arr1[i]))
                 {
-                    
+                   //If arr[i] satisfies condition of "isPrime" counter "cnt" will be incremented 
                     cnt++;
-                    //Добавляю в List простые числа
+                    //And it wil be added to List "list"
                     list.Add(arr1[i]);
                 }
             }
-
+            //Output amiunt of primes to console
             Console.WriteLine(cnt);
+            //Output content of "list" to console
             foreach (int item in list)
                 Console.Write(item + " ");
-
+            //Write command to enter a symbol to prevent closing of the console
             Console.ReadKey();
         }
-        //Создаю функцию isPrime, которая возвращает true или false 
-        //в зависомости от получаемого значения в качестве аргумента n
+        //I make function is prime that returns true or false
+        //depending on whether obtained n is prime or not
         static bool isPrime(int n)
         {
-            //Отрицательные значения не рассматриваются
+            //Negative numbers are not considered as prime
             if (n <= 0) return false;
-            //в случае полученя 1 или 2 мишем возвращаемый результат отдельно, так как 
-            //цикл будет пробегаться от 2 до 1 или от 2 до 2(здесь 2 делится на 2, будет false, должно быть true)
             if (n == 1) return false;
             if (n == 2) return true;
-            //Число Prime, если оно не делится на числа меньше квадратного корня данного числа
-            for(int i = 2; i <= Math.Ceiling(Math.Sqrt(n)); i++)
+            //Number is prime if it is nit divisible by any number less than its square root
+            //In case n==1 or n==2 we write condition separately, because 
+            //cycle will run from 2 to 1 or from 2 to 2( 2 is divisible by 2 => false, but the right answer should be true)
+            for (int i = 2; i <= Math.Ceiling(Math.Sqrt(n)); i++)
                 if (n % i == 0) return false;
-            //Если число проходит проверку(не делится) то возвращаю true
+            //If the number passes the test(not divisible), return true 
             return true;
         }
     }
