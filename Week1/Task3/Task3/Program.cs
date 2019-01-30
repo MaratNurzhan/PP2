7495a2af038c1864a6a127a2334837c24c314ed6
Nurzhan <nurzhan.marat.2000@mail.ru>
2019-01-30 12:48:24 +0600

    Comment_15

--- Week1/Task3/Task3/Program.cs
+++ Week1/Task3/Task3/Program.cs
@@ -10,20 +10,25 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            
+            //Convert the input string into int and place in variable n
             int n = int.Parse(Console.ReadLine());
             //string str=Console.ReadLine();
             //string[] arr=str.Split();
+
             string[] arr = Console.ReadLine().Split();
             int[] arr1 = new int[n];
             for(int i=0; i<arr.Length; i++)
             {
+                //Every element of arr is of type string
+                //Convert into int using "int.Parse"
+
                 int n1 = int.Parse(arr[i]);
                 arr1[i] = n1;
             }
-
+            //using foreach cycle we output every element el twice 
             foreach(var el in arr1)
             {
+                //using "Write" not to move to the next line(new array will output in one line)
                 Console.Write($"{el} {el} ");
             }
             Console.ReadKey();
