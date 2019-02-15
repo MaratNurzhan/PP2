984bbeb76b46052795edaf54c57b8cf15b8e87df
Nurzhan <nurzhan.marat.2000@mail.ru>
2019-02-15 22:36:50 +0600

    changes and comments

--- Week2/Task2/Task2/Program.cs
+++ Week2/Task2/Task2/Program.cs
@@ -11,35 +11,45 @@ namespace Task2
     {
         static bool Prime(int a)
         {
+            //cycle from 2 to 1 is wrong
             if (a == 1)
                 return false;
             if (a == 2)
                 return true;
-                
-            for(int i=2; i<Math.Sqrt(a); i++)
-            {
+           
+            //if a is divisible by any number less than sqrt(a) it is not prime=>false    
+            for(int i=2; i<=Math.Ceiling(Math.Sqrt(a)); i++)
+            
                 if (a % i == 0)
                     return false;
-            }
+            
+            //if it pass check then it is prime
             return true;
         }
 
 
         static void Main(string[] args)
         {
+            //Open stream to the file
             StreamReader sr = new StreamReader(@"C: \Users\Lenovo\Desktop\rcs\files\input_1.txt");
+            //Read the line from file and put it in string s
             string s = sr.ReadLine();
+            //split string into array of integers(devide by sign in brackets)
             string[] arr = s.Split();
+            //initilaize List to put there all primes
             List<int>list = new List<int>();
             foreach(var el in arr)
             {
+                //Convert to int
                 int n = int.Parse(el);
-
+                //if satisfies function Prime()
+                //add to List list
                 if (Prime(n))
                     list.Add(n);
             }
+            //close the stream
             sr.Close();
-
+            //output in other file the content of list
             StreamWriter sw = new StreamWriter(@"C: \Users\Lenovo\Desktop\rcs\files\input_2.txt");
             foreach(var l in list)
             {
--- Week2/Task4/Task4/Program.cs
+++ Week2/Task4/Task4/Program.cs
@@ -12,22 +12,27 @@ namespace Task4
         static void Main(string[] args)
         {
            
-
+            //Take path to directory where program is executed
             string commonPath = Directory.GetCurrentDirectory();
             string path = Path.Combine(commonPath + "Path");
+            //Create directory in the following path if it exists
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
+            //get path to direcotry where we create file
             string path1 = Path.Combine(commonPath + "Path1");
+            //cheack if such path exists
             if (!Directory.Exists(path1))
                 Directory.CreateDirectory(path1);
-            string filename = "transport.txt";
+            string filename = "transport";
+            //if file with such path exists we delete it 
             if (File.Exists(Path.Combine(path1 + filename)))
                 File.Delete(Path.Combine(path1 + filename));
-
+           
             using (StreamWriter sw = new StreamWriter(Path.Combine(path + filename)))
             {                
                 sw.Write(Path.Combine(path + filename));
             }
+            //We move file from old path to new path
             File.Move(Path.Combine(path + filename), Path.Combine(path1 + filename));
 
             Console.ReadKey();
