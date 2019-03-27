39aeb8b46c48c9281dc9d52e00a9afc81660ea07
Nurzhan <nurzhan.marat.2000@mail.ru>
2019-03-27 20:30:34 +0600

    init

---
+++ Week7/Task1/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[4];
+            for (int i = 1; i <= 3; i++)
+            {
+                threads[i] = new Thread(PrintName);
+                threads[i].Name = "Thread" + " " + i.ToString();
+
+                threads[i].Start();
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static void PrintName()
+        {
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine(Thread.CurrentThread.Name + " ");
+        }
+    }
+}
+
---
+++ Week7/Task2/Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Task2
+{
+    class MyThread
+    {
+        public Thread threadField;
+
+        public MyThread(string name)
+        {
+            threadField = new Thread(StartThread);
+            threadField.Name = name;
+            threadField.Start();
+        }
+
+
+
+        public void StartThread()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " " + "выводит" + " " + (i + 1));
+                Thread.Sleep(0);
+            }
+            Console.WriteLine(Thread.CurrentThread.Name + " " + "завершился");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyThread t1 = new MyThread("Thread 1");
+            MyThread t2 = new MyThread("Thread 2");
+
+
+            t1.StartThread();
+            t2.StartThread();
+
+            Console.Read();
+        }
+    }
+}
